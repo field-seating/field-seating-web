0cdca0896f052ab0d49cf128e867aa1c2fdf4c4b
Wendell Liu <wendell.liu@houzz.com>
2022-04-12 16:57:26 +0800

    feat: proxy for api

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const apiHost = process.env.API_HOST;
+
 const nextConfig = {
   reactStrictMode: true,
+  serverRuntimeConfig: {},
+
+  async rewrites() {
+    return [
+      // Proxy to Backend
+      {
+        source: '/api/:path*',
+        destination: `${apiHost}/api/:path*`,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
