f416454eb8d9d2266c1b6894252864e5b22b102f
Wendell Liu <wendell.liu@houzz.com>
2022-04-22 11:18:09 +0800

    feat: function item

--- components/profile/function-list/index.jsx
+++ components/profile/function-list/index.jsx
@@ -1,7 +1,21 @@
 import { Box } from '@chakra-ui/react';
 
+import FunctionItem from './function-item';
+
 const FunctionList = () => {
-  return <Box>FunctionList</Box>;
+  return (
+    <Box display="flex" flexDir="column">
+      <FunctionItem href="/profile">個人資訊</FunctionItem>
+      <FunctionItem href="/profile">管理密碼</FunctionItem>
+      <FunctionItem
+        onClick={() => {
+          console.log('click');
+        }}
+      >
+        登出
+      </FunctionItem>
+    </Box>
+  );
 };
 
 export default FunctionList;
