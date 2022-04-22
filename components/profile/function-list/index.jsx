023bd50440cd41fd89a521536d4f3c8d1430081d
Wendell Liu <wendell.liu@houzz.com>
2022-04-22 11:23:54 +0800

    feat: logout

--- components/profile/function-list/index.jsx
+++ components/profile/function-list/index.jsx
@@ -1,19 +1,27 @@
+import { useCallback, useContext } from 'react';
+import { useActor } from '@xstate/react';
 import { Box } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
+
+import { GlobalStateContext } from 'lib/contexts/globalState';
 
 import FunctionItem from './function-item';
 
 const FunctionList = () => {
+  const { authService } = useContext(GlobalStateContext);
+  const [, sendToAuthService] = useActor(authService);
+  const router = useRouter();
+
+  const logout = useCallback(() => {
+    sendToAuthService('LOGOUT');
+    router.reload();
+  }, [sendToAuthService, router]);
+
   return (
     <Box display="flex" flexDir="column">
       <FunctionItem href="/profile">個人資訊</FunctionItem>
       <FunctionItem href="/profile">管理密碼</FunctionItem>
-      <FunctionItem
-        onClick={() => {
-          console.log('click');
-        }}
-      >
-        登出
-      </FunctionItem>
+      <FunctionItem onClick={logout}>登出</FunctionItem>
     </Box>
   );
 };
