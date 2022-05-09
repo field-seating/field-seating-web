302201a30e44dfbc5012178cdf980a1b544a49f0
Wendell Liu <wendell.liu@houzz.com>
2022-05-09 17:52:56 +0800

    feat: Prompt

---
+++ components/ui/prompt/index.jsx
@@ -0,0 +1,61 @@
+import PropTypes from 'prop-types';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  Text,
+} from '@chakra-ui/react';
+import { isNil } from 'ramda';
+
+import Button from '../button';
+
+const Prompt = ({
+  title,
+  description,
+  cancelText,
+  submitText,
+  onClose,
+  isOpen,
+}) => {
+  const hasDescription = !isNil(description);
+
+  return (
+    <Drawer placement="bottom" onClose={onClose} isOpen={isOpen}>
+      <DrawerOverlay />
+      <DrawerContent>
+        <DrawerHeader borderBottomWidth={hasDescription && '1px'}>
+          {title}
+        </DrawerHeader>
+        {description && (
+          <DrawerBody>
+            <Text>{description}</Text>
+          </DrawerBody>
+        )}
+        <DrawerFooter>
+          <Button variant="outline" mr={4} onClick={onClose}>
+            {cancelText}
+          </Button>
+          <Button variant="solid">{submitText}</Button>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  );
+};
+Prompt.propTypes = {
+  isOpen: PropTypes.bool,
+  onClose: PropTypes.func,
+  title: PropTypes.string,
+  description: PropTypes.string,
+  cancelText: PropTypes.string,
+  submitText: PropTypes.string,
+};
+
+Prompt.defaultProps = {
+  cancelText: '取消',
+  submitText: '確定',
+};
+
+export default Prompt;
