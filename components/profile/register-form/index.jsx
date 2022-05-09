5f2d3b5426e551e039898d22afa4fea8dc7ef46e
Wendell Liu <wendell.liu@houzz.com>
2022-05-09 19:05:12 +0800

    refactor: registor form

--- components/profile/register-form/index.jsx
+++ components/profile/register-form/index.jsx
@@ -7,6 +7,12 @@ import { isNil } from 'ramda';
 import Button from 'components/ui/button';
 import Link from 'components/ui/link';
 import useSnackbar from 'components/ui/snackbar';
+import {
+  selectDisabled,
+  selectLoading,
+  selectSuccess,
+  selectFailure,
+} from 'lib/machines/form';
 
 import Field from 'components/input-actor-field';
 import machine from './machine';
@@ -31,23 +37,22 @@ const RegisterForm = () => {
   const snackbar = useSnackbar();
   const router = useRouter();
 
-  const globalErrorMsg = current.context.globalErrorMsg;
-  const responseData = current.context.responseData;
-  const isIdle = current.matches('idle');
-  const isLoading = current.matches('loading');
+  const isDisabled = selectDisabled(current);
+  const isLoading = selectLoading(current);
 
   useEffect(() => {
-    if (globalErrorMsg) {
+    const globalErrorMsg = current.context.globalErrorMsg;
+    if (selectFailure(current)) {
       snackbar({ text: globalErrorMsg, variant: 'error' });
+      return;
     }
-  }, [snackbar, globalErrorMsg]);
 
-  useEffect(() => {
-    if (!isNil(responseData)) {
+    if (selectSuccess(current)) {
       snackbar({ text: '註冊成功，已寄出認證信' });
       router.push('/profile');
+      return;
     }
-  }, [snackbar, responseData, router]);
+  }, [current, snackbar, router]);
 
   return (
     <>
@@ -66,7 +71,7 @@ const RegisterForm = () => {
         <Box display="flex" mt={10} justifyContent="flex-end">
           <Button
             isLoading={isLoading}
-            isDisabled={!isIdle}
+            isDisabled={isDisabled}
             variant="solid"
             type="submit"
             size="lg"
