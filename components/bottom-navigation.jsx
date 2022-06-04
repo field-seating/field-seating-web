b0cb0d3ac0840c5718d9936358a4239557a8bbd3
Wendell Liu <wendell.liu@houzz.com>
2022-06-04 16:17:27 +0800

    feat: render func in bottom navigation

--- components/bottom-navigation.jsx
+++ components/bottom-navigation.jsx
@@ -6,6 +6,34 @@ import { defaultTo, prop, find, compose } from 'ramda';
 import { tabList } from 'lib/constants/route';
 import matchRoute from 'lib/utils/match-route';
 
+const renderButton = (tab, isActive) => {
+  return (
+    <NextLink key={tab.id} href={tab.pathname} passHref>
+      <IconButton
+        as="a"
+        flex="1"
+        _hover={{
+          bg: 'primary.main',
+        }}
+        _active={{
+          bg: undefined,
+          color: 'onPrimary.main',
+        }}
+        _focus={{
+          boxShadow: undefined,
+        }}
+        icon={<tab.Icon />}
+        aria-label={tab.label}
+        borderRadius={0}
+        h="56px"
+        bg="primary.main"
+        color="onPrimary.transparent"
+        isActive={isActive}
+      />
+    </NextLink>
+  );
+};
+
 const BottomNavigation = () => {
   const router = useRouter();
 
@@ -19,31 +47,7 @@ const BottomNavigation = () => {
 
   return (
     <Box as="nav" display="flex">
-      {tabList.map((tab) => (
-        <NextLink key={tab.id} href={tab.pathname} passHref>
-          <IconButton
-            as="a"
-            flex="1"
-            _hover={{
-              bg: 'primary.main',
-            }}
-            _active={{
-              bg: undefined,
-              color: 'onPrimary.main',
-            }}
-            _focus={{
-              boxShadow: undefined,
-            }}
-            icon={<tab.Icon />}
-            aria-label={tab.label}
-            borderRadius={0}
-            h="56px"
-            bg="primary.main"
-            color="onPrimary.transparent"
-            isActive={tab.id === matchId}
-          />
-        </NextLink>
-      ))}
+      {tabList.map((tab) => renderButton(tab, tab.id === matchId))}
     </Box>
   );
 };
