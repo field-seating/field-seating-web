e1c2e51ff488164ca8ee91d154bde6df6f7a710c
Wendell Liu <wendell.liu@houzz.com>
2022-06-04 18:07:42 +0800

    feat: image uploader

--- components/upload/image-uploader.jsx
+++ components/upload/image-uploader.jsx
@@ -1,14 +1,49 @@
-import { useCallback } from 'react';
+import { useCallback, useContext } from 'react';
 import { CloudUpload } from '@mui/icons-material';
 import { Box } from '@chakra-ui/react';
+import Router from 'next/router';
 
 import BottomNavigationButton from 'components/ui/bottom-navigation-button';
+import ImageUploadContext from 'lib/contexts/image-upload';
+import useSnackbar from 'components/ui/snackbar';
+
+const FILES_LIMIT = 3;
+
+const isFilesValid = (files) => {
+  if (files.length > FILES_LIMIT) {
+    return { valid: false, message: '請至多上傳三張圖片' };
+  }
+
+  return { valid: true };
+};
 
 const ImageUploader = ({ isActive }) => {
-  const onChange = useCallback((e) => {
-    e.preventDefault();
-    console.log(e.target.files);
-  }, []);
+  const { setImages } = useContext(ImageUploadContext);
+  const snackbar = useSnackbar();
+
+  const onChange = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      const files = e.target.files;
+      console.log(files);
+
+      if (files.length === 0) {
+        return;
+      }
+
+      const { valid, message } = isFilesValid(files);
+
+      if (!valid) {
+        snackbar({ text: message, variant: 'error' });
+        return;
+      }
+
+      setImages(files);
+      Router.push('/upload');
+    },
+    [setImages, snackbar]
+  );
 
   return (
     <Box flex={1}>
