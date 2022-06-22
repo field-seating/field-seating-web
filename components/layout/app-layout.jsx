5d547f748384d5f2b8a202d67357e012e1c92088
Wendell Liu <cuk.bas@gmail.com>
2022-06-22 22:43:56 +0800

    feat: bind transition for loading

--- components/layout/app-layout.jsx
+++ components/layout/app-layout.jsx
@@ -1,28 +1,39 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Box, Spinner } from '@chakra-ui/react';
+import { useSelector } from '@xstate/react';
+
+import useAuth from 'lib/hooks/user-auth';
+import { GlobalStateContext } from 'lib/contexts/global-state';
+import { selectActive } from 'lib/machines/loading-machine';
+
 import BottomNavigation from '../bottom-navigation';
 import DefaultHead from './default-head';
-import useAuth from 'lib/hooks/user-auth';
 
 const AppLayout = ({ children }) => {
   useAuth();
+
+  const { loadingService } = useContext(GlobalStateContext);
+
+  const isLoading = useSelector(loadingService, selectActive);
 
   return (
     <Box>
       <DefaultHead />
       <Box as="main" h="100vh" pb="56px" position="relative">
-        <Box
-          bg="onSurface.40"
-          position="absolute"
-          w="100%"
-          h="100%"
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-          zIndex="100"
-        >
-          <Spinner size="lg" />
-        </Box>
+        {isLoading && (
+          <Box
+            bg="onSurface.40"
+            position="absolute"
+            w="100%"
+            h="100%"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+            zIndex="100"
+          >
+            <Spinner size="lg" />
+          </Box>
+        )}
         {children}
       </Box>
       <Box
