9e5f51c5e53aae7f2227f5345ebb44d99cf769c1
Wendell Liu <wendell.liu@houzz.com>
2022-07-07 18:08:53 +0800

    feat: select space

---
+++ components/browse/space-selector.jsx
@@ -0,0 +1,74 @@
+import { useContext, useCallback, useEffect } from 'react';
+import { useActor } from '@xstate/react';
+import { Box, useDisclosure } from '@chakra-ui/react';
+import { always, any, ifElse, isNil } from 'ramda';
+
+import { useFetchSpaces } from 'lib/fetch/fields/list-spaces';
+import { useFetchField } from 'lib/fetch/fields/get-field';
+import { useFetchZone } from 'lib/fetch/fields/get-zone';
+import { GlobalStateContext } from 'lib/contexts/global-state';
+import SpaceViewer from 'components/space-viewer';
+import AppBar from 'components/ui/app-bar';
+import ZoneCriteriaDrawer from 'components/zone-criteria-drawer';
+
+const anyNil = any(isNil);
+const renderTitle = ifElse(
+  anyNil,
+  always(''),
+  ([field, zone]) => `${field.name} ${zone.name}`
+);
+
+const SpaceSelector = () => {
+  const { browsePhotosService } = useContext(GlobalStateContext);
+  const [browsePhotosState, sendToBrowsePhotosActor] =
+    useActor(browsePhotosService);
+
+  const { zoneId, fieldId } = browsePhotosState.context;
+
+  const onSpaceSelect = useCallback(
+    (spaceId) => {
+      sendToBrowsePhotosActor({ type: 'SELECT_SPACE', spaceId });
+    },
+    [sendToBrowsePhotosActor]
+  );
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const onSave = useCallback(
+    ({ fieldId, orientationId, levelId, zoneId }) => {
+      sendToBrowsePhotosActor({
+        type: 'SAVE_SPACE_CRITERIA',
+        fieldId,
+        orientationId,
+        levelId,
+        zoneId,
+      });
+      onClose();
+    },
+    [sendToBrowsePhotosActor, onClose]
+  );
+
+  const { data: spaces } = useFetchSpaces(zoneId);
+  const { data: field } = useFetchField(fieldId);
+  const { data: zone } = useFetchZone(zoneId);
+
+  const title = renderTitle([field, zone]);
+
+  useEffect(() => {
+    sendToBrowsePhotosActor('INIT');
+  }, [sendToBrowsePhotosActor]);
+
+  return (
+    <>
+      <Box display="flex" flexDir="column" height="100%">
+        <AppBar title={title} hasMenu onMenu={onOpen} />
+        <Box flex="1" width="100%" overflowX="auto" pt={[4, 8]} px={[4, 8]}>
+          <SpaceViewer spaces={spaces || []} onSpaceSelect={onSpaceSelect} />
+        </Box>
+      </Box>
+      <ZoneCriteriaDrawer isOpen={isOpen} onClose={onClose} onSave={onSave} />
+    </>
+  );
+};
+
+export default SpaceSelector;
