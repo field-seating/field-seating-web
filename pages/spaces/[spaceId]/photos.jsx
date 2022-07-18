2429d741dc99087227b6c530240a7027c6500ba4
Wendell Liu <cuk.bas@gmail.com>
2022-07-18 16:34:11 +0800

    feat: enable SSR

--- pages/spaces/[spaceId]/photos.jsx
+++ pages/spaces/[spaceId]/photos.jsx
@@ -1,12 +1,28 @@
 import { Box } from '@chakra-ui/react';
+import { SWRConfig } from 'swr';
 
+import getSpacePhotos from 'lib/fetch/spaces/get-photos';
 import SpacePhotos from 'components/space-photos';
 
-const SpacePhotosPage = () => {
+export async function getServerSideProps({ query }) {
+  const { spaceId } = query;
+  const photosData = await getSpacePhotos(spaceId);
+
+  return {
+    props: {
+      fallback: {
+        [`/api/spaces/${spaceId}/photos`]: photosData,
+      },
+    },
+  };
+}
+const SpacePhotosPage = ({ fallback }) => {
   return (
-    <Box px={[4, 16]} py={4}>
-      <SpacePhotos />
-    </Box>
+    <SWRConfig value={{ fallback }}>
+      <Box px={[4, 16]} py={4}>
+        <SpacePhotos />
+      </Box>
+    </SWRConfig>
   );
 };
 
