4206cdacf831d3116febe5094351ba2b68bb751c
Wendell Liu <cuk.bas@gmail.com>
2022-07-19 22:43:09 +0800

    feat: add loading bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { inspect } from '@xstate/inspect';
 import { SWRConfig } from 'swr';
+import { useRouter } from 'next/router';
+import NProgress from 'nprogress';
 
 import { GlobalStateProvider } from 'lib/contexts/global-state';
 import AppLayout from 'components/layout/app-layout';
 import theme from 'lib/theme/customTheme';
 
+import 'styles/nprogress.css';
 import 'styles/app.css';
 
 const appHeightFix = () => {
@@ -31,7 +34,26 @@ if (typeof window !== 'undefined' && process.env.NODE_ENV !== 'production') {
 }
 
 function MyApp({ Component, pageProps }) {
+  const Router = useRouter();
   const getLayout = Component.getLayout || ((page) => page);
+
+  useEffect(() => {
+    NProgress.configure({ showSpinner: false });
+
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    Router.events.on('routeChangeStart', handleRouteStart);
+    Router.events.on('routeChangeComplete', handleRouteDone);
+    Router.events.on('routeChangeError', handleRouteDone);
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      Router.events.off('routeChangeStart', handleRouteStart);
+      Router.events.off('routeChangeComplete', handleRouteDone);
+      Router.events.off('routeChangeError', handleRouteDone);
+    };
+  }, [Router]);
 
   return (
     <SWRConfig
