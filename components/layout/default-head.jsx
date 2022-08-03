7d0b3be713c18f963a41f5c48d2968b090479d19
Wendell Liu <wendell.liu@houzz.com>
2022-08-04 01:21:55 +0800

    feat: favicon

--- components/layout/default-head.jsx
+++ components/layout/default-head.jsx
@@ -5,6 +5,7 @@ const DefaultHead = () => (
     <title>球場坐座</title>
     <meta name="description" content="分享球場視野的最友善平台" />
     <link rel="icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="/favicon.png" />
   </Head>
 );
 
