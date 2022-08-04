275fa3e257cbb969641e339501aa80403f11beff
Wendell Liu <wendell.liu@houzz.com>
2022-08-04 18:53:00 +0800

    feat: og title desc

--- components/layout/default-head.jsx
+++ components/layout/default-head.jsx
@@ -1,16 +1,20 @@
 import Head from 'next/head';
 
-const DefaultHead = () => (
-  <Head>
-    <title>球場坐座</title>
-    <meta
-      name="description"
-      content="透過群眾上傳，提升看球體驗。照片更多，共享更多看球視野！操作更快，手機上傳一指搞定！"
-    />
-    <link rel="icon" href="/favicon.ico" />
-    <link rel="icon" type="image/png" href="/favicon.png" />
-    <link rel="manifest" href="/manifest.json" />
-  </Head>
-);
+const DefaultHead = () => {
+  const title = '球場坐座';
+  const desc =
+    '透過群眾上傳，提升看球體驗。照片更多，共享更多看球視野！操作更快，手機上傳一指搞定！';
 
+  return (
+    <Head>
+      <title></title>
+      <meta name="description" content={desc} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={desc} />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+      <link rel="manifest" href="/manifest.json" />
+    </Head>
+  );
+};
 export default DefaultHead;
