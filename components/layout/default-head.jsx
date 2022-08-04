34d4cd58d49dc4750c7e6ed06fbe12d115290774
Wendell Liu <wendell.liu@houzz.com>
2022-08-04 13:07:04 +0800

    feat: add meta description

--- components/layout/default-head.jsx
+++ components/layout/default-head.jsx
@@ -3,7 +3,10 @@ import Head from 'next/head';
 const DefaultHead = () => (
   <Head>
     <title>球場坐座</title>
-    <meta name="description" content="分享球場視野的最友善平台" />
+    <meta
+      name="description"
+      content="透過群眾上傳，提升看球體驗。照片更多，共享更多看球視野！操作更快，手機上傳一指搞定！"
+    />
     <link rel="icon" href="/favicon.ico" />
     <link rel="icon" type="image/png" href="/favicon.png" />
   </Head>
